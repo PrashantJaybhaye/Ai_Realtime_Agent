e2787bd87067c033f915de39911a98031b691ecb
Prashant <prashan1837d@gmail.com>
2025-07-05 21:20:09 +0530

    implement auth and storage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,15 @@
 import Header from '@/components/Header'
+import { isAuthenticated } from '@/lib/actions/auth.action'
+import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react'
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
+const RootLayout = async ({ children }: { children: ReactNode }) => {
+    const isUserAuthenticated = await isAuthenticated();
+
+    if (!isUserAuthenticated) redirect('/sign-in')
     return (
         <div className='root-layout'>
-            <Header/>
+            <Header />
             {children}
         </div>
     )
--- components/login-form.tsx
+++ components/login-form.tsx
@@ -11,6 +11,9 @@ import {
 import { toast } from "sonner"
 import FormField from "./FormField"
 import { useRouter } from "next/navigation"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { auth } from "@/firebase/client"
+import { signIn, signUp } from "@/lib/actions/auth.action"
 
 
 const authFormSchema = (type: FormType) => {
@@ -38,20 +41,50 @@ export function LoginForm({
     },
   })
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       if (type === "sign-up") {
-        console.log('SIGN UP', values)
-        toast.success("You’re all set! Sign in to continue.", { position: "top-right" })
+        const { name, email, password } = values;
+
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password
+        })
+
+        if (!result?.success) {
+          toast.error(result?.message);
+          return;
+        }
+
+        toast.success("You’re all set! Sign in to continue.")
         router.push('/sign-in')
       } else {
-        console.log("SIGN IN", values)
-        toast.success("Authentication successful. You’re now logged in.", { position: "top-right" })
+
+        const { email, password } = values
+
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+        const idToken = await userCredential.user.getIdToken();
+
+        if (!idToken) {
+          toast.error('Sign in failed')
+          return;
+        }
+
+        await signIn({
+          email, idToken,
+        })
+
+        toast.success("Authentication successful. You’re now logged in.")
         router.push('/')
       }
     } catch (error) {
       console.log(error);
-      toast.error(`There was error: ${error}`, { position: "top-right" })
+      toast.error(`There was error: ${error}`)
     }
   }
 
